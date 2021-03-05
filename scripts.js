141c0ab44c9d3cb570c058f0cb4161012209f13f
hooster <germainmusul@schoolap.com>
2021-03-05 13:46:59 +0200

    First commit Pinterest

---
+++ scripts.js
@@ -0,0 +1,23 @@
+// document.querySelector('.container').addEventListener('click', function(){
+//     document.querySelector('.box').classList.toggle('move');
+// });
+
+// document.querySelector('.show-modal').addEventListener('click', function(){
+//     document.querySelector('.modal').style.display = 'block';
+// });
+
+// document.querySelector('.close-modal').addEventListener('click', function(){
+//     document.querySelector('.modal').style.display = 'none';
+// });
+
+document.querySelector('.container').addEventListener('click', function(){
+    document.querySelector('.box').classList.toggle('move');
+});
+
+document.querySelector('.open-modal').addEventListener('click', function(){
+    document.querySelector('.modal').style.display = 'block';
+})
+
+document.querySelector('.close-modal').addEventListener('click', function(){
+    document.querySelector('.modal').style.display = 'none';
+})
